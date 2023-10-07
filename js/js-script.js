3fc1c88273e26b32b1423b00f55b7c0401941c90
Arifin123-creator <ahmadarifinrizkiakbar@gmail.com>
2023-10-07 11:50:37 +0800

    update Booking and login form

--- js/js-script.js
+++ js/js-script.js
@@ -1,49 +1,132 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
+ // script.js
+ document.addEventListener("DOMContentLoaded", function () {
     const spesialisSelect = document.getElementById("spesialis");
     const dokterSelect = document.getElementById("dokter");
     const bookingForm = document.getElementById("bookingForm");
     const hasilTabel = document.getElementById("hasilTabel");
     const downloadButton = document.getElementById("downloadButton");
 
-    spesialisSelect.addEventListener("change", function () {
-        // Dapatkan nilai spesialis yang dipilih
-        const selectedSpesialis = spesialisSelect.value;
+    // Data rumah sakit, spesialis, dan dokter dalam bentuk array
+    const dataRumahSakit = [
+        {
+            name: "RSUD Boven Digeol",
+            spesialis: [
+                {
+                    name: "Spesialis X",
+                    dokter: ["Dokter 1X", "Dokter 2X", "Dokter 3X"],
+                },
+                {
+                    name: "Spesialis Y",
+                    dokter: ["Dokter 1Y", "Dokter 2Y", "Dokter 3Y"],
+                },
+                {
+                    name: "Spesialis U",
+                    dokter: ["Dokter 1Y", "Dokter 2Y", "Dokter 3Y"],
+                },
+            ],
+        },
+        {
+            name: "RSUD Kwaingga",
+            spesialis: [
+                {
+                    name: "Spesialis Z",
+                    dokter: ["Dokter 1Z", "Dokter 2Z", "Dokter 3Z"],
+                },
+                {
+                    name: "Spesialis Y",
+                    dokter: ["Dokter 1Y", "Dokter 2Y", "Dokter 3Y"],
+                },
+                {
+                    name: "Spesialis U",
+                    dokter: ["Dokter 1Y", "Dokter 2Y", "Dokter 3Y"],
+                },
+                
+            ],
+        },
+        {
+            name: "RSUD Kabupaten Mappi",
+            spesialis: [
+                {
+                    name: "Spesialis P",
+                    dokter: ["Dokter 1P", "Dokter 2P", "Dokter 3P"],
+                },
+                {
+                    name: "Spesialis Y",
+                    dokter: ["Dokter 1Y", "Dokter 2Y", "Dokter 3Y"],
+                },
+                {
+                    name: "Spesialis U",
+                    dokter: ["Dokter 1Y", "Dokter 2Y", "Dokter 3Y"],
+                },
+               
+            ],
+        },
+        {
+            name: "RSUD Paniai",
+            spesialis: [
+                {
+                    name: "Spesialis Q",
+                    dokter: ["Dokter 1Q", "Dokter 2Q", "Dokter 3Q"],
+                },
+                {
+                    name: "Spesialis Y",
+                    dokter: ["Dokter 1Y", "Dokter 2Y", "Dokter 3Y"],
+                },
+                {
+                    name: "Spesialis U",
+                    dokter: ["Dokter 1Y", "Dokter 2Y", "Dokter 3Y"],
+                },
+               
+            ],
+        },
+    ];
 
-        // Hapus semua opsi dokter saat ini
-        dokterSelect.innerHTML = "";
+    const rumahSakitSelect = document.getElementById("rumahSakit");
 
-        // Tambahkan opsi dokter sesuai dengan spesialis yang dipilih
-        switch (selectedSpesialis) {
-            case "Spesialis A":
-                addDokterOptions(["--..--","Dokter X", "Dokter Y", "Dokter Z"]);
-                break;
-            case "Spesialis B":
-                addDokterOptions(["--..--","Dokter P", "Dokter Q", "Dokter R"]);
-                break;
-            case "Spesialis C":
-                addDokterOptions(["--..--","Dokter F", "Dokter G", "Dokter H"]);
-                break;
-            default:
-                addDokterOptions([]);
-        }
+    rumahSakitSelect.addEventListener("change", function () {
+        const selectedHospital = rumahSakitSelect.value;
+        const selectedHospitalData = dataRumahSakit.find(
+            (hospital) => hospital.name === selectedHospital
+        );
+
+        // Populate Spesialis dropdown based on selected hospital
+        spesialisSelect.innerHTML = '<option value="#">-- Select Specialist --</option>';
+        selectedHospitalData.spesialis.forEach((spesialisData) => {
+            const option = document.createElement("option");
+            option.value = spesialisData.name;
+            option.textContent = spesialisData.name;
+            spesialisSelect.appendChild(option);
+        });
     });
 
-    // Fungsi untuk menambahkan opsi dokter ke elemen select
-    function addDokterOptions(dokterList) {
-        for (const dokter of dokterList) {
+    spesialisSelect.addEventListener("change", function () {
+        const selectedHospital = rumahSakitSelect.value;
+        const selectedSpesialis = spesialisSelect.value;
+        const selectedHospitalData = dataRumahSakit.find(
+            (hospital) => hospital.name === selectedHospital
+        );
+
+        // Find selected spesialis data
+        const selectedSpesialisData = selectedHospitalData.spesialis.find(
+            (spesialisData) => spesialisData.name === selectedSpesialis
+        );
+
+        // Populate Dokter dropdown based on selected spesialis
+        dokterSelect.innerHTML = '<option value="#">-- Select Doctor --</option>';
+        selectedSpesialisData.dokter.forEach((dokterName) => {
             const option = document.createElement("option");
-            option.value = dokter;
-            option.textContent = dokter;
+            option.value = dokterName;
+            option.textContent = dokterName;
             dokterSelect.appendChild(option);
-        }
-    }
+        });
+    });
 
     bookingForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
         const nama = document.getElementById("nama").value;
         const nomor = document.getElementById("nomor").value;
+        const rumahSakit = document.getElementById("rumahSakit").value;
         const spesialis = document.getElementById("spesialis").value;
         const dokter = document.getElementById("dokter").value;
         const tanggal = document.getElementById("tanggal").value;
@@ -53,6 +136,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const bookingData = {
             nama: nama,
             nomor: nomor,
+            rumahSakit: rumahSakit,
             spesialis: spesialis,
             dokter: dokter,
             tanggal: tanggal,
@@ -64,20 +148,21 @@ document.addEventListener("DOMContentLoaded", function () {
         const newRow = hasilTabel.insertRow();
         const cellNama = newRow.insertCell(0);
         const cellNomor = newRow.insertCell(1);
-        const cellSpesialis = newRow.insertCell(2);
-        const cellDokter = newRow.insertCell(3);
-        const cellTanggal = newRow.insertCell(4);
-        const cellWaktu = newRow.insertCell(5);
-        const cellAlamat = newRow.insertCell(6);
+        const cellRumahSakit = newRow.insertCell(2);
+        const cellSpesialis = newRow.insertCell(3);
+        const cellDokter = newRow.insertCell(4);
+        const cellTanggal = newRow.insertCell(5);
+        const cellWaktu = newRow.insertCell(6);
+        const cellAlamat = newRow.insertCell(7);
 
         cellNama.innerHTML = nama;
         cellNomor.innerHTML = nomor;
+        cellRumahSakit.innerHTML = rumahSakit;
         cellSpesialis.innerHTML = spesialis;
         cellDokter.innerHTML = dokter;
         cellTanggal.innerHTML = tanggal;
         cellWaktu.innerHTML = waktu;
         cellAlamat.innerHTML = alamat;
-        
 
         // Menampilkan tombol Download PDF
         downloadButton.style.display = "block";
@@ -89,10 +174,17 @@ document.addEventListener("DOMContentLoaded", function () {
     downloadButton.addEventListener("click", function () {
         // Konversi elemen hasil booking menjadi PDF (seperti yang telah dijelaskan sebelumnya)
         const element = document.getElementById("hasilBooking");
-        const pdfOptions = { margin: 10, filename: "booking_dokter.pdf", image: { type: "jpeg", quality: 0.98 } }
-        
+        const pdfOptions = {
+            margin: 10,
+            filename: "booking_dokter.pdf",
+            image: { type: "jpeg", quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: {
+                unit: "mm",
+                format: "a4",
+                orientation: "landscape",
+            },
+        };
         html2pdf().from(element).set(pdfOptions).save();
-
     });
-
 });
--- js/js-scriptLogin.js
+++ js/js-scriptLogin.js
@@ -2,37 +2,6 @@
 const wrapper = document.querySelector(".wrapper"),
 signupHeader = document.querySelector(".signup header"),
 loginHeader = document.querySelector(".login header");
-const loginForm = document.getElementById('login-form');
-const message = document.getElementById('message');
-
-loginForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-
-  try {
-    const response = await fetch('/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    });
-
-    const data = await response.json();
-
-    if (response.ok) {
-      message.innerText = data.message;
-      // Redirect atau tampilkan halaman beranda setelah login berhasil
-    } else {
-      message.innerText = data.message;
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
-});
-
 
 
 loginHeader.addEventListener("click", () => {
@@ -42,22 +11,72 @@ signupHeader.addEventListener("click", () => {
 wrapper.classList.remove("active");
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("loginForm");
+// Login
+const loginForm = document.getElementById("login-Form");
+const message = document.getElementById("message");
 
-    loginForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-        // Lakukan validasi login di sini, contoh sederhana:
-        if (username === "pengguna" && password === "password") {
-            // Jika login berhasil, arahkan ke halaman booking
-            window.location.href = "booking.html";
-        } else {
-            // Jika login gagal, tampilkan pesan kesalahan
-            alert("Login gagal. Periksa kembali username dan password Anda.");
-        }
+  try {
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
     });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      message.innerText = data.message;
+      window.location.href = "index.html";
+      // Redirect atau tampilkan halaman beranda setelah login berhasil
+    } else {
+      message.innerText = data.message;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
+
+
+
+// Register
+const regisForm = document.getElementById("regisForm");
+const message1 = document.getElementById("message1");
+
+regisForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const fullname = document.getElementById("fullname").value;
+  const email = document.getElementById("email1").value;
+  const password = document.getElementById("password1").value;
+
+  try {
+    const response = await fetch("http://localhost:5000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password, fullname }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      message1.innerText = data.message;
+      window.location.href = "login.html #login";
+      // Redirect atau tampilkan halaman beranda setelah login berhasil
+    } else {
+      message1.innerText = data.message;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
