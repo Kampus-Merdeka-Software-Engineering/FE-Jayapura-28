efe6bc4016b1ca024f82e5ddee2ca7b068b8759b
Arifin123-creator <ahmadarifinrizkiakbar@gmail.com>
2023-10-10 14:51:19 +0800

    Update js-scriptLogout.js

--- js/js-scriptLogout.js
+++ js/js-scriptLogout.js
@@ -1,41 +1,4 @@
-// // JavaScript di halaman Anda
-// const logoutButton = document.getElementById('logoutButton');
 
-// // Fungsi untuk mengatur tampilan tombol "Log Out" berdasarkan status login
-// // Mendapatkan token dari penyimpanan lokal (localStorage)
-// const token = localStorage.getItem('token');
-
-
-
-// logoutButton.addEventListener('click', async () => {
-//   try {
-//     // Mengirim permintaan logout ke server
-//     const response = await fetch('https://be-jayapura-28-production-015b.up.railway.app/logout', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         // Sertakan token atau informasi otentikasi lainnya jika diperlukan
-//         // Anda mungkin perlu menyimpan token di localStorage atau cookie untuk penggunaan logout
-//         'Authorization': `Bearer ${localStorage.getItem('token')}`,
-//       },
-//     });
-
-//     if (response.ok) {
-//       // Logout berhasil, hapus token atau data sesi di client-side
-//       localStorage.removeItem('token');
-
-//       // Redirect atau tampilkan pesan sukses
-//       window.location.href = 'login.html#login';
-//     } else {
-//       // Logout gagal, tampilkan pesan kesalahan
-//       console.error('Logout gagal');
-//     }
-//   } catch (error) {
-//     console.error('Error:', error);
-//   }
-// });
-
-// Frontend code
 
 // Mendapatkan tombol logout
 const logoutButton = document.getElementById('logoutButton');
@@ -68,11 +31,4 @@ logoutButton.addEventListener('click', () => {
   // Tampilkan tombol login (opsional)
 });
 
-// // Event listener untuk tombol login (opsional)
-// if (loginButton) {
-//   loginButton.addEventListener('click', () => {
-//     // Implementasikan logika login atau navigasi ke halaman login
-//     // ...
-//   });
-// }
 });
